fa81e0bed20c4e3cafc30aa67e6568125a6bdad8
Andy <andy.silv@yandex.ru>
2016-01-31 04:20:24 +0300

    forgot it

--- fast_layout.py
+++ fast_layout.py
@@ -7,33 +7,19 @@ import numpy as np
 from osgeo import gdal
 import tempfile
 import time
-from layout_builder import util, hgt_downloader, gdal_merge
+from layout_builder import util, hgt_downloader, gdal_merge, ProgressDialog
 from PySide import QtCore, QtGui
 import re
 import struct
 import ctypes
-
 
 support_directory = os.path.dirname(os.path.realpath(__file__)) + os.sep + u'layout_builder'
 support_directory = str(support_directory.encode(sys.getfilesystemencoding()), 'utf8')
 gdal.UseExceptions()
 
 
-class SpecificFolderFileHandler(srtm.FileHandler):
-    def __init__(self, cache_folder=None):
-        self.cache_folder = cache_folder
-        print(cache_folder)
-        if cache_folder is not None:
-            if not os.path.exists(cache_folder):
-                os.makedirs(cache_folder)
-
-    def get_srtm_dir(self):
-        if self.cache_folder is None:
-            return super.get_srtm_dir()
-        return self.cache_folder
-
 def test():
-    handler = SpecificFolderFileHandler(os.path.join(get_path_in_chunk(), '.srtm'))
+    handler = util.SpecificFolderFileHandler(os.path.join(get_path_in_chunk(), '.srtm'))
     elevation_data = srtm.get_data(file_handler=handler)
     h = elevation_data.get_elevation(57.1, 95.1, approximate=True)
     print(h)
@@ -101,7 +87,7 @@ def write_model_file(f, points, normals, faces):
 
 
 def build_mesh(output_file, min_latitude, max_latitude, min_longitude, max_longitude, lat_step, long_step, need_download=True):
-    handler = SpecificFolderFileHandler(os.path.join(get_path_in_chunk(), '.srtm'))
+    handler = util.SpecificFolderFileHandler(os.path.join(get_path_in_chunk(), '.srtm'))
     elevation_data = srtm.get_data(file_handler=handler)
 
     get_height = lambda x,y: elevation_data.get_elevation(x, y, approximate=True)
@@ -178,8 +164,8 @@ def get_chunk_bounds(chunk):
 def align_cameras(chunk, min_latitude, min_longitude, max_latitude, max_longitude):
     if chunk.transform.scale is None:
         chunk.transform.scale = 1
-        chunk.transform.rotation = PhotoScan.Matrix([[1,0,0], [0,1,0], [0,0,1]])
-        chunk.transform.translation = PhotoScan.Vector([0,0,0])
+        chunk.transform.rotation = ps.Matrix([[1,0,0], [0,1,0], [0,0,1]])
+        chunk.transform.translation = ps.Vector([0,0,0])
 
     delta_latitude_scale_to_meters = 40008000 / 360
 
@@ -206,70 +192,6 @@ def revert_changes(chunk):
     for c in chunk.cameras:
         c.transform = None
 
-# @return full name of result hgt file
-def apply_egm_offset(hgt_file):
-    egm_tif_file = os.path.join(os.getcwd(), 'geoids', 'egm96-15.tif')
-    try:
-        tif = gdal.Open(egm_tif_file)
-    except:
-        print('Couldn\'t find "egm96-15.tif"')
-        return None
-
-    tif_array = tif.ReadAsArray()
-
-    band1 = tif.GetRasterBand(1).ReadAsArray()
-
-    file_size = os.path.getsize(hgt_file)
-    cols = rows = int(math.sqrt(file_size / 2))
-    step = 1.0 / (cols - 1) # 1 / 1200 for the world, 1 / 3600 for US
-
-    file_name = os.path.basename(hgt_file)
-    m = re.match('([NS])(\d+)([WE])(\d+)', file_name)
-
-    # so that 90N will be 0, and 90S will be 180
-    lat_sign = -1 if m.group(1) == 'N' else 1
-    lon_sign = -1 if m.group(3) == 'W' else 1
-    start_lat = 90 + lat_sign * int(m.group(2))
-    start_lon = 180 + lon_sign * int(m.group(4))
-
-    # output file name will be like N53E069_wgs84.hgt
-    output_path = os.path.dirname(hgt_file)
-    output_name = str(file_name.split('.')[0]) + u'_wgs84.hgt'
-    hgt_wgs_file = os.path.join(output_path, output_name)
-
-    processed_files = os.path.join(output_path, 'processed.txt')
-    for line in processed_files:
-        if line.strip() == hgt_file:
-            return
-
-    data = ctypes.create_string_buffer(2 * rows * cols)
-    offset = 0
-
-    with open(hgt_file, "rb") as in_file:
-        for r in range(rows):
-            for c in range(cols):
-                lon = start_lon + lon_sign * r * step
-                lat = start_lat + lat_sign * c * step
-                buf = in_file.read(2)
-
-                val = struct.unpack('>h', buf)[0]
-                # find corresponing pixel in offsets map
-                offset_map_col = int(720 / 180 * lat)
-                offset_map_row = int(1440 / 360 * lon)
-                offset_sample = band1[offset_map_col][offset_map_row]
-
-                if val != -32768:
-                    val += offset_sample
-
-                struct.pack_into(">h", data, offset, int(val))
-                offset += 2
-
-    with open(hgt_file, "wb") as out_file:
-        out_file.write(data.raw)
-
-    with open(processed_files, "a") as f:
-        f.write(hgt_file + '\n')
-
 
 class DemImporter(QtCore.QObject):
     def __init__(self, parent=None):
@@ -292,53 +214,34 @@ class DemImporter(QtCore.QObject):
             print("Save project before importing dem!")
             return
 
-        progress = QtGui.QProgressDialog(self.translator.translate('dlg', "Downloading DEMs..."),
-                                             self.translator.translate('dlg', "Cancel"), 0, 100, None)
-
         def download_canceled():
             downloader.stop_running()
             while not downloader.isFinished():
                 time.sleep(0.1)
 
-        progress.setWindowTitle(self.translator.translate('dlg', 'Download progress'))
-        progress.setWindowModality(QtCore.Qt.WindowModal)
-        progress.canceled.connect(download_canceled)
-        progress.resize(progress.sizeHint() + QtCore.QSize(30, 0))
+        progress_dialog = ProgressDialog.ProgressDialog()
+        progress_dialog.canceled.connect(download_canceled)
 
         hgts_folder = os.path.join(get_path_in_chunk(), '.srtm')
         tif_folder = os.path.join(get_path_in_chunk(), 'geotifs')
-        handler = SpecificFolderFileHandler(hgts_folder)
-        elevation_data = srtm.get_data(file_handler=handler)
 
         downloader = hgt_downloader.HGTDownloader(min_latitude, min_longitude,
-                                                   max_latitude, max_longitude, elevation_data)
-        downloader.update_progress.connect(progress.setValue)
+                                                   max_latitude, max_longitude, hgts_folder)
+        downloader.update_current_progress.connect(progress_dialog.set_current_progress)
+        downloader.update_overall_progress.connect(progress_dialog.set_overall_progress)
+        downloader.set_current_task_name.connect(progress_dialog.set_current_label_text)
         downloader.start()
-        progress.show()
+        progress_dialog.show()
 
         while downloader.isRunning():
             QtGui.qApp.processEvents()
             time.sleep(0.1)
 
-        progress.hide()
         if downloader.stopped:
             revert_changes(chunk)
             return
 
-        hgt_names = ''
-        for hgt_file in downloader.hgt_names:
-            full_hgt_name = os.path.join(hgts_folder, hgt_file)
-            apply_egm_offset(full_hgt_name)
-            hgt_names += full_hgt_name + ' '
-
-        merged_tif = os.path.join(hgts_folder, 'result.tif')
-
-        clip_bounds = ' -te {} {} {} {} '.format(
-                min_longitude, min_latitude, max_longitude, max_latitude)
-        print(clip_bounds)
-        gdal_command = 'gdalwarp ' + clip_bounds + hgt_names + merged_tif
-        os.system(gdal_command) # + ' "+proj=longlat +ellps=WGS84"'
-        chunk.importDem(merged_tif)
+        chunk.importDem(downloader.merged_tif)
         '''
         full_tif_names = downloader.get_full_tif_names()
 
@@ -387,9 +290,5 @@ def run_camera_alignment():
 translator = get_translator(QtGui.QApplication.instance())
 ps.app.addMenuItem(translator.translate(
         'dlg', "Tools/Import/Import SRTM DEM..."), run_import)
-
-try:
-    ps.app.addMenuItem(translator.translate(
-        'dlg', "Workflow/Apply Vertical Camera Alignment..."), run_camera_alignment)
-except Exception as e:
-    print(e)
+ps.app.addMenuItem(translator.translate(
+    'dlg', "Workflow/Apply Vertical Camera Alignment..."), run_camera_alignment)
